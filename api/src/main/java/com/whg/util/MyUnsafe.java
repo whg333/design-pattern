c9a662f884bf82433ff241c21185ee9b307b35c7
wanghonggang <wanghonggang@hoolai>
2022-02-15 15:40:51 +0800

    封装sun.misc.Unsafe

---
+++ api/src/main/java/com/whg/util/MyUnsafe.java
@@ -0,0 +1,95 @@
+package com.whg.util;
+
+import java.lang.reflect.Field;
+
+/**
+ * 封装sun.misc.Unsafe
+ */
+public class MyUnsafe {
+
+    static sun.misc.Unsafe unsafe;
+    static {
+        try {
+            Field unsafeField = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
+            unsafe = (sun.misc.Unsafe) unsafeField.get(null);
+        } catch (Exception e) {
+            e.printStackTrace(); //ignore
+        }
+    }
+
+    public static void putBoolean(Object obj, long fieldOffset, boolean val) {
+        unsafe.putBoolean(obj, fieldOffset, val);
+    }
+    public static void putInt(Object obj, long fieldOffset, int val) {
+        unsafe.putInt(obj, fieldOffset, val);
+    }
+    public static void putLong(Object obj, long fieldOffset, long val) {
+        unsafe.putLong(obj, fieldOffset, val);
+    }
+    public static void putShort(Object obj, long fieldOffset, short val) {
+        unsafe.putShort(obj, fieldOffset, val);
+    }
+    public static void putByte(Object obj, long fieldOffset, byte val) {
+        unsafe.putByte(obj, fieldOffset, val);
+    }
+    public static void putChar(Object obj, long fieldOffset, char val) {
+        unsafe.putChar(obj, fieldOffset, val);
+    }
+    public static void putFloat(Object obj, long fieldOffset, float val) {
+        unsafe.putFloat(obj, fieldOffset, val);
+    }
+    public static void putDouble(Object obj, long fieldOffset, double val) {
+        unsafe.putDouble(obj, fieldOffset, val);
+    }
+    public static void putObject(Object obj, long fieldOffset, Object val) {
+        unsafe.putObject(obj, fieldOffset, val);
+    }
+
+    public static boolean getBoolean(Object obj, long fieldOffset) {
+        return unsafe.getBoolean(obj, fieldOffset);
+    }
+    public static int getInt(Object obj, long fieldOffset) {
+        return unsafe.getInt(obj, fieldOffset);
+    }
+    public static long getLong(Object obj, long fieldOffset) {
+        return unsafe.getLong(obj, fieldOffset);
+    }
+    public static short getShort(Object obj, long fieldOffset) {
+        return unsafe.getShort(obj, fieldOffset);
+    }
+    public static byte getByte(Object obj, long fieldOffset) {
+        return unsafe.getByte(obj, fieldOffset);
+    }
+    public static char getChar(Object obj, long fieldOffset) {
+        return unsafe.getChar(obj, fieldOffset);
+    }
+    public static float getFloat(Object obj, long fieldOffset) {
+        return unsafe.getFloat(obj, fieldOffset);
+    }
+    public static double getDouble(Object obj, long fieldOffset) {
+        return unsafe.getDouble(obj, fieldOffset);
+    }
+    public static Object getObject(Object obj, long fieldOffset) {
+        return unsafe.getObject(obj, fieldOffset);
+    }
+
+    public static long objectFieldOffset(Field field) {
+        return unsafe.objectFieldOffset(field);
+    }
+
+    public static long staticFieldOffset(Field field) {
+        return unsafe.staticFieldOffset(field);
+    }
+    public static Object staticFieldBase(Field field) {
+        return unsafe.staticFieldBase(field);
+    }
+    public static Object allocateInstance(Class<?> cls) {
+        try {
+            return unsafe.allocateInstance(cls);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ api/src/test/java/com/whg/test/Test.java
@@ -0,0 +1,86 @@
+package com.whg.test;
+
+import com.whg.util.MyUnsafe;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Test {
+
+    private static final AtomicInteger count = new AtomicInteger();
+
+    public static void main(String[] args) throws Exception{
+        testFinal1();
+        new Test().testFinal2();
+    }
+
+    private static void testFloat(){
+        float f = -1.27f;
+        System.out.println(Float.toHexString(f));
+        System.out.println(Integer.toBinaryString(Float.floatToIntBits(f)));
+    }
+
+    private static void testSchedule(){
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        AtomicLong consume = new AtomicLong(new Date().getTime());
+        Runnable task = () -> {
+            long now = new Date().getTime();
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            count.incrementAndGet();
+            System.out.println("one done..."+count.get()+", consume="
+                    +(now-consume.get()));
+            consume.set(now);
+        };
+
+        //service.scheduleAtFixedRate(task, 2, 2, TimeUnit.SECONDS);
+        service.scheduleWithFixedDelay(task, 2, 2, TimeUnit.SECONDS);
+    }
+
+    public static final char[] value1 = new char[]{'a', 'b', 'c',};
+    public final char[] value2 = new char[]{'d', 'e', 'f'};
+
+    private static void testFinal1() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        Field field = Test.class.getDeclaredField("value1");
+
+        boolean useReflect = false;
+        if(useReflect){
+            field.setAccessible(true);
+
+            Field modifiers = Field.class.getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(field,field.getModifiers()&~Modifier.FINAL);
+
+            field.set(null, new char[]{'1', '2', '3'});
+        }else{
+            MyUnsafe.putObject(Test.class, MyUnsafe.staticFieldOffset(field), new char[]{'1', '2', '3'});
+        }
+
+        System.out.println(value1);
+    }
+
+    public void testFinal2() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        Field field = Test.class.getDeclaredField("value2");
+
+        boolean useReflect = false;
+        if(useReflect) {
+            field.setAccessible(true);
+            field.set(this, new char[]{'4', '5', '6'});
+        }else{
+            MyUnsafe.putObject(this, MyUnsafe.objectFieldOffset(field), new char[]{'4', '5', '6'});
+        }
+
+        System.out.println(this.value2);
+    }
+
+}
